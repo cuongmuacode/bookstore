c5772de3b06fecc3972301daaee1483a86619755
Lưu Mạnh Cường <cuonglmsinx@gmail.com>
2025-03-18 17:05:47 +0700

    git init project book store

---
+++ src/Acme.BookStore.Web/Menus/BookStoreMenus.cs
@@ -0,0 +1,9 @@
+﻿namespace Acme.BookStore.Web.Menus;
+
+public class BookStoreMenus
+{
+    private const string Prefix = "BookStore";
+
+    public const string Home = Prefix + ".Home";
+    public const string Book = Prefix + ".Book";
+}
